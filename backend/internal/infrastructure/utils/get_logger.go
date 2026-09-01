eb7bc97b5273e0886c5c3cf7d6d40a4e057441c4
Lata Amenu <amelatbay@gmail.com>
2026-09-01 15:58:18 +0300

    helper funcrrions are added

---
+++ backend/internal/infrastructure/utils/get_logger.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+// GetLogger returns a logrus Entry containing common request metadata.
+func GetLogger(c *gin.Context) *log.Entry {
+	traceID := c.GetString("TraceID")
+	if traceID == "" {
+		traceID = "unknown"
+	}
+
+	userID := "anonymous"
+
+	if uid, err := GetUserID(c); err == nil {
+		userID = uid.Hex()
+	}
+
+	return log.WithFields(log.Fields{
+		"trace_id":   traceID,
+		"user_id":    userID,
+		"ip":         c.ClientIP(),
+		"user_agent": c.Request.UserAgent(),
+	})
+}
